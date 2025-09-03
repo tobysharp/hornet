3aa31adae81453b5248a6b10ab3f51a80f0c7c86
Toby Sharp <tsharp@hornetnode.com>
2025-09-03 06:43:31 +0000

    Add Notify layer in hornet::util, and reroute Log methods to use NotifyLog and DefaultLogSink.

---
+++ src/hornetlib/util/notify.cpp
@@ -0,0 +1,73 @@
+#include "hornetlib/util/notify.h"
+
+#include <atomic>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <unordered_map>
+
+namespace hornet::util {
+
+DefaultLogSink& DefaultLogSink::Instance() {
+  static DefaultLogSink instance;
+  return instance;
+}
+
+void DefaultLogSink::SetOutputFile(const std::string& filename) {
+  std::lock_guard lock(mutex_);
+  file_.open(filename, std::ios::app);
+}
+
+void DefaultLogSink::EnableStdout(bool enabled) {
+  std::lock_guard lock(mutex_);
+  to_stdout_ = enabled;
+}
+
+void DefaultLogSink::operator()(const NotificationPayload& payload) {
+  std::lock_guard lock(mutex_);
+  const std::string& level = std::get<std::string>(payload.map.at("level"));
+  const std::string& message = std::get<std::string>(payload.map.at("msg"));
+  const int64_t time_us = std::get<int64_t>(payload.map.at("time_us"));
+  const std::string full = Prefix(level, time_us) + message + "\n";
+
+  if (to_stdout_) std::cout << full;
+  if (file_.is_open()) file_ << full;
+}
+
+std::string DefaultLogSink::Prefix(const std::string& level, int64_t time_us) const {
+  using namespace std::chrono;
+  const sys_time<microseconds> tp{microseconds{time_us}};
+  return "[" + std::string{level} + (level.size() < 5 ? " " : "") +
+         std::format(" {:%H:%M:%S}.{:04}", floor<seconds>(tp),
+                     (tp.time_since_epoch() % 1s).count() / 100) +
+         "] ";
+}
+
+namespace {
+NotificationSink notification_sink = [](const NotificationPayload& payload) {
+  DefaultLogSink::Instance()(payload);
+};
+}  // namespace
+
+void Notify(NotificationType type, std::string_view path, const NotificationMap& values) {
+  notification_sink({type, path, values});
+}
+
+void NotifyLog(const NotificationMap& values) {
+  Notify(NotificationType::Log, "", values);
+}
+
+void NotifyEvent(std::string_view path, const NotificationMap& values) {
+  Notify(NotificationType::Discrete, path, values);
+}
+
+void NotifyMetric(std::string_view path, const NotificationMap& values) {
+  Notify(NotificationType::Continuous, path, values);
+}
+
+void SetNotificationSink(NotificationSink sink) {
+  notification_sink = std::move(sink);
+}
+
+}  // namespace hornet::util
