277faacbe6ebc68e1409bb362b3285b07b519e7e
Toby Sharp <tsharp@hornetnode.com>
2025-10-17 03:44:06 +0000

    SingleWriter<T> for multiple lock-free readers and serialized writers.

--- src/hornetlib/data/utxo/memory_age.h
+++ src/hornetlib/data/utxo/memory_age.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "hornetlib/data/utxo/memory_run.h"
+#include "hornetlib/data/utxo/single_writer.h"
 #include "hornetlib/data/utxo/types.h"
 
 namespace hornet::data::utxo {
@@ -11,39 +12,28 @@ namespace hornet::data::utxo {
 class MemoryAge {
  public:
   using MemoryRunPtr = std::shared_ptr<const MemoryRun>;
-  using MemoryRunSet = std::vector<MemoryRunPtr>;
-  using MemoryRunSetPtr = std::shared_ptr<const MemoryRunSet>;
 
   MemoryAge(int merge_fan_in, bool is_mutable = false);
-  bool IsMutable() const {
-    return is_mutable_;
-  }
+
+  bool IsMutable() const { return is_mutable_; }
   int Query(std::span<const OutputKey> keys, std::span<OutputId> rids) const;
-  int Size() const {
-    return std::ssize(*runs_);
-  }
+  int Size() const { return std::ssize(*runs_); }
   bool IsMergeReady() const { return Size() >= merge_fan_in_; }
 
   void Append(MemoryRun&& run);
-
   void Merge(MemoryAge* dst);
 
  protected:
-  MemoryRunSetPtr Snapshot() const { return runs_; }
-  void Publish(MemoryRunSetPtr ptr) { runs_ = ptr; }
-
   const int merge_fan_in_;
   const bool is_mutable_;
-  std::atomic<MemoryRunSetPtr> runs_;
+  SingleWriter<std::vector<MemoryRunPtr>> runs_;
 };
 
 inline MemoryAge::MemoryAge(int merge_fan_in, bool is_mutable = false)
-    : merge_fan_in_(merge_fan_in),
-      is_mutable_(is_mutable),
-      runs_(std::make_shared<MemoryRunSet>()) {}
+    : merge_fan_in_(merge_fan_in), is_mutable_(is_mutable) {}
 
 inline int MemoryAge::Query(std::span<const OutputKey> keys, std::span<OutputId> rids) const {
-  const MemoryRunSetPtr snapshot = Snapshot();
+  const auto snapshot = runs_.Snapshot();
   return std::accumulate(snapshot->rbegin(), snapshot->rend(), 0,
                          [&](int sum, const MemoryRunPtr& run) {
                            if (sum == std::ssize(keys)) return sum;
@@ -52,19 +42,16 @@ inline int MemoryAge::Query(std::span<const OutputKey> keys, std::span<OutputId>
 }
 
 inline void MemoryAge::Append(MemoryRun&& run) {
-  const MemoryRunSetPtr snapshot = Snapshot();
-  auto copy = std::make_shared<MemoryRunSet>(*snapshot);
-  copy->emplace_back(std::make_shared<MemoryRun>(std::move(run)));
-  Publish(copy);
+  const auto ptr = std::make_shared<MemoryRun>(std::move(run));
+  runs_.CopyOnWrite()->push_back(ptr);
 }
 
 inline void MemoryAge::Merge(MemoryAge* dst) {
-  const MemoryRunSetPtr snapshot = Snapshot();
-  if (snapshot->size() < merge_fan_in_) return;  // Nothing to do.
-
-  const auto inputs = std::span{*snapshot}.first(merge_fan_in_);
+  if (runs_.Snapshot()->size() < merge_fan_in_) return;  // Nothing to do.
+  const auto copy = dst->runs_.CopyOnWrite();
+  const auto inputs = std::span{*copy}.first(merge_fan_in_);
   dst->Append(MemoryRun::Merge(inputs, dst->IsMutable()));
-  Publish(std::make_shared<MemoryRunSet>(snapshot->begin() + merge_fan_in_, snapshot->end()));
+  copy->erase(copy->begin(), copy->begin() + merge_fan_in_);
 }
 
 }  // namespace hornet::data::utxo
